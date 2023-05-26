046a69b879b36a2802a4fe9d1c103867d2fa1073
fbhood <mail@fabiopacifici.com>
2023-05-26 10:23:24 +0200

    add csv read on seeders

--- database/seeders/HouseSeeder.php
+++ database/seeders/HouseSeeder.php
@@ -6,6 +6,7 @@ use App\Models\House;
 use Faker\Generator as Faker;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Utilities\Helpers as Help;
 
 class HouseSeeder extends Seeder
 {
@@ -17,7 +18,8 @@ class HouseSeeder extends Seeder
     public function run(Faker $faker)
     {
 
-        for ($i = 0; $i < 10; $i++) {
+        // Option 1 con faker
+        /*         for ($i = 0; $i < 10; $i++) {
             $house = new House();
             $house->property_image = $faker->imageUrl(category: 'Houses', format: 'jpg');
             $house->reference_no = $faker->bothify('??-########');
@@ -35,6 +37,35 @@ class HouseSeeder extends Seeder
             $house->is_available = $faker->randomElement([1, 0]);
             $house->energy_rating = $faker->randomElement(['A+++', 'A++', 'B', 'C', 'D']);
             $house->save();
+        } */
+        /* Option 2 con csv */
+
+        /* Chiama la funzione getCsvData per leggere il csv  */
+
+        $data = Help::getCsvData(__DIR__ . '/houses.csv');
+        //dd($data);
+        /* cicla nell'array e genera un nuovo oggetto house a ciascuna iterazione */
+        foreach ($data as $index => $row) {
+            //dd($row, $index);
+            /* we skip the rist row as it contains the csv heading fields */
+            if ($index !== 0) {
+                $house = new House();
+                $house->property_image = $row[0];
+                $house->reference_no = $row[1];
+                $house->address = $row[2];
+                $house->post_code = $row[3];
+                $house->city = $row[4];
+                $house->state = $row[5];
+                $house->square_meters = $row[6];
+                $house->rooms = $row[7];
+                $house->bathrooms = $row[8];
+                $house->type = $row[9];
+                $house->desciption = $row[10];
+                $house->price = $row[11];
+                $house->is_available = $row[12];
+                $house->energy_rating = $row[13];
+                $house->save();
+            }
         }
     }
 }
